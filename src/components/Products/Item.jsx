ef5005e1ae2677c39e137fa31aaecf932e126977
Belen De Franchi <belendefranchi@gmail.com>
2023-02-27 11:26:13 -0300

    agrego boton ver mas para ItemDetail

--- src/components/Products/Item.jsx
+++ src/components/Products/Item.jsx
@@ -63,17 +63,16 @@ const Item = ( { product, carts, getCartList, updateCartQuantity } ) => {
 
     return (
         <Card className='m-2'>
-            <Link to={`${product.id}`} key={product.id}>
-                <Card.Link className='text-reset'>
-                    <Card.Img className='img-fluid p-0 m-0' variant="top" src={product.image} />
-                    <Card.Body>
-                        <Card.Title className='fs-5 text-secondary'>{product.title}</Card.Title>
-                        {/* <Card.Text className='p-1 text-secondary' style={{ height: '6rem', margin: '1rem' }}>{product.description}</Card.Text> */}
-                        <Card.Title className='p-1 fs-4 fw-bold text-secondary'>$ {product.price}</Card.Title>
-                    </Card.Body>
-                </Card.Link>
-            </Link>
-            <Card.Body className='d-flex px-1'>
+            <Card.Body>
+                <Card.Img className='img-fluid' variant="top" src={product.image} />
+                <Card.Title className='fs-5 text-secondary'>{product.title}</Card.Title>
+                {/* <Card.Text className='p-1 text-secondary' style={{ height: '6rem', margin: '1rem' }}>{product.description}</Card.Text> */}
+                <Card.Title className='p-1 fs-4 fw-bold text-secondary'>$ {product.price}</Card.Title>
+                <Link to={`${product.id}`} key={product.id}>
+                    <Button className='mx-1 w-75 btn' variant='success'>Ver más</Button>
+                </Link>
+            </Card.Body>
+            <Card.Footer className='d-flex px-1'>
                 <Form.Select className='mx-1 w-50 qty' id={product.id} onChange={() => getSelectValue(product.id)}>
                     <option value="1">1</option>
                     <option value="2">2</option>
@@ -83,7 +82,7 @@ const Item = ( { product, carts, getCartList, updateCartQuantity } ) => {
                     <option value="6">6</option>
                 </Form.Select>
                 <Button className='mx-1 w-75 btn' variant='success' id={product.id} onClick={() => addToCart(product, carts)}>Agregar al carrito</Button>
-            </Card.Body>
+            </Card.Footer>
         </Card>
     )
 };
